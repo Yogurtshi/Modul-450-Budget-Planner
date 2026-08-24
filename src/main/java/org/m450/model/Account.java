edde5e3de86a5c085ff8166e54df903787a882e2
ygyger <yoshi.gyger@klixa.ch>
2026-08-24 17:19:53 +0200

    feat: withdraw and deposit with test

--- src/main/java/org/m450/model/Account.java
+++ src/main/java/org/m450/model/Account.java
@@ -1,4 +1,40 @@
 package org.m450.model;
 
+import java.math.BigDecimal;
+
 public class Account {
+
+    private int id;
+    private String name;
+    private BigDecimal balance;
+    private int customerId;
+
+    public Account(int id, String name, BigDecimal balance, int customerId) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+        this.customerId = customerId;
+    }
+
+    public boolean isValidAmount(BigDecimal amount) {
+        return amount != null && amount.compareTo(BigDecimal.ZERO) != 0;
+    }
+
+    public void deposit(BigDecimal amount) {
+        if (!isValidAmount(amount) || amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Deposit amount must be positive");
+        }
+        balance = balance.add(amount);
+    }
+
+    public void withdraw(BigDecimal amount) {
+        if (!isValidAmount(amount) || amount.compareTo(BigDecimal.ZERO) >= 0) {
+            throw new IllegalArgumentException("Withdraw amount must be negative");
+        }
+        balance = balance.add(amount);
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
 }
--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -3,12 +3,13 @@ import org.m450.model.Account;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AccountTest {
 
+    // Checks if the range is positive of negative, these are both accepted
     @Test
-    void isValidAmount_withAmountAboveZero_returnsTrue() {
+    void isValidAmount_withPositiveAmount_returnsTrue() {
         Account account = new Account(
                 1,
                 "Main account",
@@ -19,5 +20,110 @@ public class AccountTest {
         boolean result = account.isValidAmount(new BigDecimal("0.01"));
 
         assertTrue(result);
+    }
+
+    @Test
+    void isValidAmount_withNegativeAmount_returnsTrue() {
+        Account account = new Account(
+                1,
+                "Main account",
+                new BigDecimal("100.00"),
+                1
+        );
+
+        boolean result = account.isValidAmount(new BigDecimal("-0.01"));
+
+        assertTrue(result);
+    }
+
+    // Edgecases 0 or null
+
+    @Test
+    void isValidAmount_withZeroAmount_returnsFalse() {
+        Account account = new Account(
+                1,
+                "Main account",
+                new BigDecimal("100.00"),
+                1
+        );
+
+        boolean result = account.isValidAmount(new BigDecimal("0"));
+
+        assertFalse(result);
+    }
+
+    @Test
+    void isValidAmount_withNullAmount_returnsFalse() {
+        Account account = new Account(
+                1,
+                "Main account",
+                new BigDecimal("100.00"),
+                1
+        );
+
+        boolean result = account.isValidAmount(null);
+
+        assertFalse(result);
+    }
+
+// Deposit Test
+    @Test
+    void deposit_withPositiveAmount_increasesBalance() {
+        Account account = new Account(
+                1,
+                "Main account",
+                new BigDecimal("100.00"),
+                1
+        );
+
+        account.deposit(new BigDecimal("50.00"));
+
+        assertEquals(new BigDecimal("150.00"), account.getBalance());
+    }
+
+    @Test
+    void deposit_withNegativeAmount_throwsIllegalArgumentException() {
+        Account account = new Account(
+                1,
+                "Main account",
+                new BigDecimal("100.00"),
+                1
+        );
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> account.deposit(new BigDecimal("-50.00"))
+        );
+        assertEquals(new BigDecimal("100.00"), account.getBalance());
+    }
+
+    // Withdraw Test
+    @Test
+    void withdraw_withPositiveAmount_decreasesBalance() {
+        Account account = new Account(
+                1,
+                "Main account",
+                new BigDecimal("100.00"),
+                1
+        );
+
+        account.withdraw(new BigDecimal("-50.00"));
+        assertEquals(new BigDecimal("50.00"), account.getBalance());
+    }
+
+    @Test
+    void withdraw_withNegativeAmount_throwsIllegalArgumentException() {
+        Account account = new Account(
+                1,
+                "Main account",
+                new BigDecimal("100.00"),
+                1
+        );
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> account.withdraw(new BigDecimal("50.00"))
+        );
+        assertEquals(new BigDecimal("100.00"), account.getBalance());
     }
 }
